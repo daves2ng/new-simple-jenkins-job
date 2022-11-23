3e1ba40a4762b405b632fb5c30e325f6b53131d1
David <daves2ng@gmail.com>
2022-11-23 17:34:49 +0000

    first commit

---
+++ src/main/java/com/techdavez/web/App.java
@@ -0,0 +1,13 @@
+package com.techdavez.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
